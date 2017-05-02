22b3080f09d2dbf9c415db94569deb384313683c
Maciej Olejniczak <olej116116@hotmail.com>
2017-05-02 18:12:46 +0200

    Implementacja metryki Hamminga

--- src/main/java/puzzleSolver/heuristic/Hamming.java
+++ src/main/java/puzzleSolver/heuristic/Hamming.java
@@ -9,11 +9,25 @@ public class Hamming implements Heuristic {
 
     @Override
     public int compare(Puzzle o1, Puzzle o2) {
-        return 0;
+        return this.calculateHeuristic(o1) - this.calculateHeuristic(o2);
     }
 
     @Override
     public int calculateHeuristic(Puzzle puzzle) {
-        return 0;
+        int result = 0;
+        int value = 1;
+        int[][] state = puzzle.getPuzzleArray();
+        for (int i = 0; i < state.length; i++) {
+            for (int j = 0; j < state[0].length; j++) {
+                if (i == state.length - 1 && j == state[0].length - 1) {
+                    break;
+                }
+                if (puzzle.getCoordsOfValue(value) != puzzle.getExpectedCoordsOfValue(value)) {
+                    result += 1;
+                }
+                value++;
+            }
+        }
+        return result;
     }
 }
